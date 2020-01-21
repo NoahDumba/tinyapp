38e60c7a6dccb33cd0636f7a01ad589c0deef2d7
Noah Dumba <noahdumba1@gmail.com>
2020-01-21 19:29:10 +0000

    added the ability to edit links without changing their shortURL

--- express_server.js
+++ express_server.js
@@ -39,14 +39,24 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls/");
 }) 
 
-app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+app.post("/urls/:shortURL", (req, res) => {
+  urlDatabase[req.params.shortURL] = req.body.newLongURL;
+  res.redirect("/urls/");
 });
 
 app.post("/urls", (req, res) => {
@@ -54,11 +64,6 @@ app.post("/urls", (req, res) => {
   urlDatabase[uniqueID] = req.body['longURL'];
   let templateVars = { shortURL: uniqueID, longURL: urlDatabase[uniqueID] };
   res.redirect("/urls/" + uniqueID);
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
